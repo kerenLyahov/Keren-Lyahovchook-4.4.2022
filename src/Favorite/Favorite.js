f40ee34cabb84a49d959e9122d1911fd47286786
kerenLyahov <kerenlyahov@gmail.com>
2022-04-10 20:09:16 +0300

    Update Favorite.js

--- src/Favorite/Favorite.js
+++ src/Favorite/Favorite.js
@@ -5,9 +5,11 @@ export default function Favorite() {
   let favor = [];
   let data = JSON.parse(window.localStorage.getItem("favorite"));
 
-  useEffect(() => {
-    favor.push(data);
-  }, [data.weatherData.key]);
+  //whenever the data will change, it will create a new child in the favorite array.
+
+  // useEffect(() => {
+  //   favor.push(data);
+  // }, [data]);
 
   return <div></div>;
 }
